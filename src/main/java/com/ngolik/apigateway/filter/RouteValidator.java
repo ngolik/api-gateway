35668284ed6c8baa743335b47bab65b9b931f9b7
ngolik <nikita.golik@softeq.com>
2023-11-27 00:31:24 +0100

    Add routes

--- src/main/java/com/ngolik/apigateway/filter/RouteValidator.java
+++ src/main/java/com/ngolik/apigateway/filter/RouteValidator.java
@@ -9,8 +9,8 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
-            "/auth/register",
-            "/auth/token",
+            "/auth/rest/register",
+            "/auth/rest/token",
             "/eureka"
     );
 
